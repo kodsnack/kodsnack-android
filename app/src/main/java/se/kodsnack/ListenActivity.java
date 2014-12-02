e5e6f1f4925fbfc13d09c7d2cfe15e41beac7c8d
Erik Jansson <erikjansson90@gmail.com>
2014-12-02 18:07:25 +0100

    Clean up left over imports.

--- app/src/main/java/se/kodsnack/ListenActivity.java
+++ app/src/main/java/se/kodsnack/ListenActivity.java
@@ -1,25 +1,11 @@
 package se.kodsnack;
 
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class ListenActivity extends Activity {
     /* Logger tag. */
     private static final String TAG = ListenActivity.class.getSimpleName();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
